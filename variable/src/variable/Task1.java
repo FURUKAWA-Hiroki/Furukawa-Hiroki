f16b8745dbadf5ec62c743566556528a9edf45a1
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-09 15:28:00 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task1.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        double A = 31.2;
+        String B = "Java研修";
+        int C = 9;
+        long D = 2147483648L;
+        String E = "才";
+
+        System.out.println( A + B + C + D + E);
+
+
+	}
+
+}
