5d7ee0b7f310d1b0641197858dc81d990ad99f9f
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-09 15:48:41 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task2.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Task2 {
+	public static void main(String[] args) {
+		String name =("古川 裕貴");
+		String X = ("です");
+		String Y = ("私の携帯番号は");
+	    String Z = ("'090-1234-5678'");
+
+	    System.out.println(name + X );
+	    System.out.println(Y + Z + X );
+}
+}
