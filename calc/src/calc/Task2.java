c46247406442c996c7fd4a6b42d98ca5fcfedab6
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-09 18:59:19 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task2.java
@@ -0,0 +1,17 @@
+package calc;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		 int x  =(10 + 12 + 13);
+         int y  =(x / 3);
+      double z  =(x % 3.0);
+
+       System.out.println("合計：" + x);
+       System.out.println("平均(3で割った数)：" + y);
+       System.out.println("平均(3.0で割った数)：" + z);
+	}
+
+}
