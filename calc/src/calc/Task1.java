aef13482af21af4d3637fc88ebf0e78f28d2494d
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-09 18:44:37 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task1.java
@@ -0,0 +1,19 @@
+package calc;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    int a =(3 + 2 * 6 + 5);
+    int b =(a + 15);
+
+    int c =(25 / 3);
+    int d =(25 % 3);
+
+    System.out.println("修正前：" + a);
+    System.out.println("修正後：" + b);
+    System.out.println("25/3の商" + c + "余り" + d);
+
+	}
+
+}
