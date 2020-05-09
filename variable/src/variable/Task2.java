952ec684c390f65beb8ba78c9cfdd30837d14a53
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-09 17:43:56 +0900

    課題提出 - 変数・定数

--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
 	public static void main(String[] args) {
 		String name =("古川 裕貴");
 		String X = ("です");
-		String Y = ("私の携帯番号は");
+		String Y = ("＼'私の携帯番号は＼'");
 	    String Z = ("'090-1234-5678'");
 
 	    System.out.println(name + X );
---
+++ variable/src/variable/Task3.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+    int intNum = 219;
+           long A =(long)intNum;
+
+    double doubleNum = 364.2;
+    float B = (float)doubleNum;
+
+    long longNum = 123456L;
+    double C = longNum;
+
+    System.out.println(A + B +C);
+	}
+}
