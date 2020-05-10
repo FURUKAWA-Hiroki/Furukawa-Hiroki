8047f6bcb24c80ab4226a821d4f92d600612d99a
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-11 01:43:18 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task4.java
@@ -0,0 +1,15 @@
+package calc;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+        int A = 192837;
+		int X = (A - 7) / 10;
+        int Y = (A % 10);
+
+		System.out.println("一の位を除く数:" + X);
+		System.out.println("一の位の数:" + Y);
+	}
+}
+
