35d933f4a1c948f5a9a10c3e64755fcabc1d4e58
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-25 17:45:47 +0900

    再提出 - Javaの基礎

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -2,11 +2,11 @@ package basic;
 public class Task2 {
     public static void main(String[] args) {
         /*
-                   コメント
-          　　　コメント
+        　　　　コメント
+        　　　　コメント
         */
         System.out.println("hogehoge");
         System.out.println("hogehoge2");
     }
 }
-//上書き保存のための文字列
+//上書き保存のための文字列 最新版
