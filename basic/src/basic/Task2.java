5130d444a1e84aa4e27078da8c0b5fe6e9da5135
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-25 17:42:19 +0900

    basic/src/basic/Task2.java

--- basic/src/basic/Task2.java
+++ basic/src/basic/Task2.java
@@ -1,12 +1,12 @@
 package basic;
 public class Task2 {
     public static void main(String[] args) {
-    /*
-          コメント
-          コメント
-    */
-    System.out.println("hogehoge");
-    System.out.println("hogehoge2");
+        /*
+                   コメント
+          　　　コメント
+        */
+        System.out.println("hogehoge");
+        System.out.println("hogehoge2");
     }
 }
 //上書き保存のための文字列
