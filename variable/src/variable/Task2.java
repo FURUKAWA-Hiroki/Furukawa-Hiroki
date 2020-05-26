e7b475f358639effa033c6465d6ab7d06dcdfd6b
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-26 18:57:35 +0900

    再提出 - 変数・定数

--- variable/src/variable/Task2.java
+++ variable/src/variable/Task2.java
@@ -2,8 +2,8 @@ package variable;
 public class Task2 {
     public static void main(String[] args) {
         String name ="古川 裕貴";
-        final String telnumber = "'090-1234-5678'";
+        final String  TELNUMBER = "'090-1234-5678'";
         System.out.println(name + "です。" );
-        System.out.println("私の電話番号は" + telnumber + "です。");
+        System.out.println("私の電話番号は" + TELNUMBER + "です。");
     }
 } //上書き保存のため 最新版
