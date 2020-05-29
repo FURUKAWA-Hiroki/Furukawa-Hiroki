1a3d7120fdd17114d9341b1ee5bfd7d79eed696b
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-29 15:52:15 +0900

    再提出 - ループ

--- loop/src/loop/Task2.java
+++ loop/src/loop/Task2.java
@@ -1,14 +1,15 @@
 package loop;
 import java.util.Scanner;
-public class Task2 {
+public class Task2{
     public static void main(String[] args) {
+    	System.out.print("数字を入力してください: ");
         Scanner sc = new Scanner(System.in);
-        int numA = sc.nextInt();  
-        System.out.println("数字を入力してください:" + numA);
+        int numA = sc.nextInt();
         sc.close();
+        System.out.println(numA);
 
         int sum = 0;
-        for(int i = 1 ; i <= numA; i ++){ 
+        for(int i = 1 ; i <= numA; i ++){
             sum += i ;
             System.out.print(i + " + ");
 
@@ -17,4 +18,4 @@ public class Task2 {
             }
         }
     }
-}
+}//再提出のためのコメント
