94900255ad123010d3920209aa815755fc78e609
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-05-30 18:21:15 +0900

    再提出 - 配列

--- array/src/array/Task1.java
+++ array/src/array/Task1.java
@@ -1,21 +1,37 @@
+//今後のために、条件式を一々言語化します。冗長になってしまい申し訳ないです。
+
 package array;
 public class Task1 {
     public static void main(String[] args) {
-
-        int[] array = new int[] {4,9,19,5,8,21,42,64,73,18,2}; 
+        int[] array = new int[] {4,9,19,5,8,21,42,64,73,18,2};
         System.out.print("配列:　[");
 
         for(int i = 0; i < array.length; i++) {
-            if(array.length < 0){
-                System.out.print(array[i] + "," );
+            System.out.print(array[i]);
+            /*iを宣言、iの値をiに代入したのち、iを今回のarraylength(arery[10]2)の値までループ処理した後
+              array内で加算していきます。
+            */
+
+            if(i < array.length -1){
+                System.out.print(",");
             }else {
-                System.out.println( "]");
+                System.out.println("]");
             }
         }
+        /*for文内大ブロック内にて、iの 値が加算されています。
+                          そして、if文にて、iの値がarraylengthの値の前の値（-1にて指定）した場合、,を入力する分岐処理をしています。
+                          また、else文にてiの値がlengthの値の前の値（-1にて指定）以外の場合、則ちarraylenghのとき"}"
+                          を入力する分岐処理をしています。
+        */
+
         int sum = 0;
-        for(int b : array) {
-            sum += b;
+        for(int i2 : array) {
+            sum += i2;
         }
         System.out.println("合計:" + sum);
+        /*iの値は既に前のfor文で指定しているので、for文にて別の変数(i2)を指定しています。
+                        拡張for文にて(inti2 = 0; i <array; I++)と同じ処理をしています。
+          　　　　　合計の値を一度初期化した変数sumに代入した後、自動加算されてきたiの和として算出しています。 
+        */
     }
-}
+} 
--- array/src/array/Task2.java
+++ array/src/array/Task2.java
@@ -1,26 +1,41 @@
+//今後のために、条件式を一々言語化します。冗長になってしまい申し訳ないです。
+
 package array;
-public class Task2 {
-
-    public static void main(String[] args) {
-        
-         int[]m = new int[7];
-         for(int i = 0; i < m.length; i++) {
-            
+public class Task2{
+    public static void main(String[] args){
+        int[] array = new int [8];
+        for(int i = 0; i < array.length; i++){
+            array[i] = 8 - i;
         }
-         for(int j = 0; j < m.length; j++) {
-             if(j == m .length-1) {
-                System.out.println(m[j]);
-            }else {
-                System.out.print(m[j] + ",");
+        //変数iをaray.length(今回の要素数iまで++にてインクリメントした後
+        //配列に格納されたiを7から減算していくことを示しています。
+
+        for(int i = 1; i <= array.length && i > 0 ;i++){
+            if(i < array.length){
+                System.out.print(array[i] + ",");
+            }else{
+                System.out.println();
             }
         }
+        /*   for文で初期化した後、array.lengthの値までループ処理することを
+                            示しています。
+                               また、iを0より大きいと指定することで、無限ループになることを防ぐ条件指定を
+                            しています。
+            　　　　　　   if文にてiがarray.lengthの値より小さいときに、
+            　　　　　　減算されていく配列に格納されたiの値を示しています。
+             else文にて1までiが減算されたときに改行を促しています。
+         */
 
-        for(int n = m.length; n >=0; n--) {
-            if(n == 1) {
-                System.out.print(m[n--]);
-            }else {
-                System.out.print(m[n-1] + ",");
+        for(int i2 = 1 ; i2 <= array.length ; i2++) {
+            if(i2 < array.length) {
+                System.out.print(i2 + ",");
+            }else{
+                System.out.println();
             }
         }
+        //for文にてi2の値を指定。array.lengthの値までインクリメントする
+        // ループ処理をしています。if文にてi2の値がarray.lengthより小さいとき
+        //出力するように促しています。elseがないと条件式がエラーとなるので入力の上、
+        //改行を促しています。
     }
 }
