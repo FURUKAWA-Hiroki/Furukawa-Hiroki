e2290d5fe62090e2389543c4e25960491dee97a9
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-06-12 16:03:45 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -1,12 +1,12 @@
 package instance;
 public class Person{
-          public String name;
-          public int age ;
+    public String name;
+    public int age ;
     //アクセス制限を持つクラスをTask1以下に作成しています。
     //属性を宣言した上で、フィールドを宣言しています。
-        public static void myProfile(String name, int age) {
-            myProfile("古川裕貴です。", 26);
-        }
+    public static void myProfile(String name, int age) {
+        myProfile("古川裕貴です。", 26);
+    }
 }
 /*Task3用にmyPogfileのメソッドを作成しています。
      ・前回提出時はPersonをクラス名にしたところ、
--- instance/src/instance/Task3.java
+++ instance/src/instance/Task3.java
@@ -1,7 +1,8 @@
 package instance;
 public class Task3 {
-    public static void myProfile() {
-        System.out.println();
-
+    public static void myProfile(String name, int age) {
+         System.out.println("私の名前は" + name +"です。");
+         System.out.println("年齢は" +  age + "です。");
+         System.out.println("よろしくお願いします。");
     }
 }
