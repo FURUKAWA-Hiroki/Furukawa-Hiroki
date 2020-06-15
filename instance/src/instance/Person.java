3cf7c4938bf6306c48d94b1e23431c2f85845a5d
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-06-15 16:37:32 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -1,20 +1,10 @@
 package instance;
 //課題1
 public class Person{
+    // 「public static void main(String[] args) {}」を宣言しないとJavaアプリケーションを
+    //実行できないので宣言します。
+    public static void main(String[] args) {}
     public String name;
     public int age ;
-    //アクセス制限を持つクラスをTask1以下に作成しています。
-    //属性を宣言した上で、フィールドを宣言しています。
-    public static void myProfile(String name, int age) {
-        myProfile("古川裕貴です。", 26);
-       // System.out.println("私の名前は" + name +"です。");
-        //System.out.println("年齢は" +  age + "です。");
-        //System.out.println("よろしくお願いします。");
-    }
+    //アクセス制限を持つクラスをTask1ブロック以下に作成しています。
 }
-/*Task3用にmyPogfileのメソッドを作成しています。
-     ・前回提出時はPersonをクラス名にしたところ、
-  Javaアプリケーションにて実行出来なかったため、
-  class名をTask１で記述でしたが、
-  　今回はデバッグにて確認済です。
-*/
--- instance/src/instance/Task3.java
+++ instance/src/instance/Task3.java
@@ -1,8 +1,11 @@
 package instance;
 public class Task3 {
+    public static void main(String[] args) {
+       myProfile("古川裕貴です。", 26);
+    }
     public static void myProfile(String name, int age) {
-         System.out.println("私の名前は" + name +"です。");
-         System.out.println("年齢は" +  age + "です。");
-         System.out.println("よろしくお願いします。");
+        System.out.println("私の名前は" + name +"です。");
+        System.out.println("年齢は" +  age + "です。");
+        System.out.println("よろしくお願いします。");
     }
 }
