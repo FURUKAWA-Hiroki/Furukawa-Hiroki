7400527d84e035bb43085fad42c783672e748a81
FURUKAWA-Hiroki <gevoge.999.banzisai0hgauma.5@gmail.com>
2020-06-17 17:50:57 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -1,10 +1,16 @@
 package instance;
 //課題1
 public class Person{
-    // 「public static void main(String[] args) {}」を宣言しないとJavaアプリケーションを
-    //実行できないので宣言します。
-    public static void main(String[] args) {}
-    public String name;
-    public int age ;
+    String name;
+    int age ;
     //アクセス制限を持つクラスをTask1ブロック以下に作成しています。
+    //また、アクセス修飾子を外しております。
+
+    //以下課題3のコードです。
+    public void myProfile() {
+        System.out.println("私の名前は" + this.name + "です。");
+        System.out.println("年齢は" + this.age + "です。");
+        System.out.println("よろしくお願いします。");
+        
+    }
 }
--- instance/src/instance/Task3.java
+++ instance/src/instance/Task3.java
@@ -1,11 +1,11 @@
 package instance;
 public class Task3 {
     public static void main(String[] args) {
-       myProfile("古川裕貴です。", 26);
-    }
-    public static void myProfile(String name, int age) {
-        System.out.println("私の名前は" + name +"です。");
-        System.out.println("年齢は" +  age + "です。");
-        System.out.println("よろしくお願いします。");
+        Person hiroki = new Person();
+        hiroki.name = "古川 裕貴";
+        hiroki.age = 26 ;
+
+        hiroki.myProfile();
+        //Personクラスの課題を呼び出しています。
     }
 }
